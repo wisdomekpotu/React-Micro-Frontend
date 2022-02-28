e431fb7280065d8148ebf25a4226aaaabb679ec0
The_Tech_Lead <58194343+wisdomekpotu@users.noreply.github.com>
2022-02-28 18:21:48 +0100

    s

--- packages/host/config/webpack.prod.js
+++ packages/host/config/webpack.prod.js
@@ -9,7 +9,7 @@ const prodConfig = {
   mode: 'production',
   output: {
     filename: '[name].[contenthash].js',
-    // publicPath: '/host/latest/',
+    publicPath: '/host/latest/',
   },
   plugins: [
     new ModuleFederationPlugin({
