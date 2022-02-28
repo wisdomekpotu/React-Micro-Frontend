96dccd9e01d225b22f6cfb56f25ff58ae824950b
The_Tech_Lead <58194343+wisdomekpotu@users.noreply.github.com>
2022-02-28 17:23:10 +0100

    recent changes

--- packages/product/config/webpack.prod.js
+++ packages/product/config/webpack.prod.js
@@ -6,7 +6,7 @@ const prodConfig = {
   mode: 'production',
   output: {
     filename: '[name].[contenthash].js',
-    publicPath: '/productlatest/',
+    // publicPath: '/productlatest/',
   },
   plugins: [
     new ModuleFederationPlugin({
